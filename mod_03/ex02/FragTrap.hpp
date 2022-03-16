a32d23ea25ab22b34d38a9bcac387f168c525215
Lukas <glukas94@gmail.com>
2022-03-16 21:57:09 +0100

    mod03 inherence :D

--- mod_03/ex02/FragTrap.hpp
+++ mod_03/ex02/FragTrap.hpp
@@ -6,8 +6,22 @@
 /*   By: lugonzal <lugonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 21:49:36 by lugonzal          #+#    #+#             */
-/*   Updated: 2022/03/16 21:49:37 by lugonzal         ###   ########.fr       */
+/*   Updated: 2022/03/16 21:56:56 by lugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef	FRAGTRAP_HPP
+#define	FRAGTRAP_HPP
 
+class FragTrap : public ClapTrap
+{
+	public:
+		FragTrap ( void );
+		FragTrap ( std::string );
+		FragTrap ( const FragTrap& );
+
+		void	highFivesGuys(void);
+	private:
+};
+
+#endif

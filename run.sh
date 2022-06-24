cd8d27fbd0c00c6e7c511605b42b0a4a7b74f016
lugonzalg <glukas94@gmail.com>
2022-06-24 21:26:38 +0200

    refactor inheritance

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ "$1" = "run" ]]; then
     #docker run -it --rm --volume /etc/passwd:/etc/passwd:ro --volume /etc/group:/etc/group:ro --user 1000:1000 -v ${PWD}:/tmp/ cpp /bin/bash
     docker run -it --rm --name cpp_env -v ${PWD}:/tmp/ cpp /bin/bash

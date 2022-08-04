8b82fa3048084fc0b65699d8c6887ba506d52c3f
Lukas <glukas94@gmail.com>
2022-08-04 21:29:10 +0200

    refactor

--- mod_06/ex01/main.cpp
+++ mod_06/ex01/main.cpp
@@ -1,38 +1,11 @@
 #include <stdlib.h>
 
 #include "Data.hpp"
-
-static size_t	fill(const unsigned char *src, unsigned char *dst, size_t len) {
-	size_t i;
-	size_t j;
-
-	i = 0;
-	j = 0;
-	for (i = 0; i < len; i++)
-		dst[i] = src[i];
-	for (j = 0; j < 4; j += 2) {
-		dst[i + j] = 0xff;
-		dst[i + j + 1] = 0x00;
-	}
-	return j + i;
-}
+#include <iostream>
 
 static uintptr_t	serialize(Data *ptr) {
-	unsigned char	*serialBuffer;
-	char	*ref;
-	size_t	sz;
-	int		num;
-
-	num = ptr->getAge();
-	sz = ptr->getName().size();
-	sz += sizeof(ptr->getAge());
-	sz += sizeof(ptr->getAccuracy());
-
-	serialBuffer = new unsigned char[sz + 9];
-	sz = fill(reinterpret_cast<const unsigned char *>(ptr->getName().c_str()), serialBuffer, ptr->getName().size());
-	sz = fill(static_cast<const unsigned char*>(static_cast<const void*>(&num)), serialBuffer + sz, sizeof(int));
-	//fill(reinterpret_cast<const unsigned char *>(ptr->getAccuracy()), serialBuffer, sizeof(float));
-	return reinterpret_cast<uintptr_t>(serialBuffer);
+	char	*serialized;
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 static Data	*deserialize(uintptr_t raw) {
@@ -41,9 +14,13 @@ static Data	*deserialize(uintptr_t raw) {
 }
 
 int main() {
-	Data test("Lukas", 42, 42.42f);
+	Data test("Lukass", 42, -1.42f);
 	uintptr_t	serialized;
+	char	*ser;
 
 	serialized = serialize(&test);
+	ser = reinterpret_cast<char *>(serialized);
+
+	std::cout << sizeof(serialized);
 	return 0;
 }

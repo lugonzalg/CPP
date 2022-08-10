4e6789ba050707867adfd9133181753c47ccf2f2
Lukas <glukas94@gmail.com>
2022-08-10 17:07:48 +0200

    new branchcd

--- mod_06/ex01/main.cpp
+++ mod_06/ex01/main.cpp
@@ -1,12 +1,22 @@
 #include <stdlib.h>
+#include <fstream>
 
 #include "Data.hpp"
 
 static uintptr_t	serialize(Data *ptr) {
-	return reinterpret_cast<uintptr_t>(serialBuffer);
+	uintptr_t	retval;
+	std::fstream	file;
+
+	retval = 0;
+	file.open("porros.bin", std::ios::binary);
+	if (!file.is_open())
+		return (uintptr_t)NULL;
+	file.write(reinterpret_cast<char *>(ptr), sizeof(Data));
+	return retval;
 }
 
 static Data	*deserialize(uintptr_t raw) {
+	(void)raw;
 	return new Data;
 }
 

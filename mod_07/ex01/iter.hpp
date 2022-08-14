d173e5f9aa4a4f1664df8fbadee933b57b98d412
Lukas Gonzalez <lukas@techfriendly.es>
2022-08-14 13:17:54 +0200

    ex01 tested, fixed

--- mod_07/ex01/iter.hpp
+++ mod_07/ex01/iter.hpp
@@ -1,20 +1,24 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
+#include <iostream>
+
+//src https://stackoverflow.com/questions/34940023/c11-for-loop-in-a-template-function
+
 template <class T>
-void	trans(T &ptr) {
-	ptr = 42;
+void	trans(T& pos) {
+	pos = 42;
 }
 
-/*template<>
-void	trans(float *pos) {
+template<>
+void	trans(float &pos) {
 	pos = 42.42f;
-}*/
+}
 
 template <class T>
-void	iter(T &src, int sz, void(*ft)(T &)) {
+void	iter(T * src, int sz, void(*ft)(T &)) {
 	for (int i = 0; i < sz; i++)
-		ft(&src[i]);
+		ft(src[i]);
 }
 
 #endif
--- mod_07/ex01/main.cpp
+++ mod_07/ex01/main.cpp
@@ -2,40 +2,26 @@
 
 #include <iostream>
 
-class Awesome
-{
-	public:
-		Awesome( void ) : _n( 42 ) { return; } int get( void ) const { return this->_n; } private:
-		int _n;
-};
-
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
-template< typename T >
-void print( T const & x ) { std::cout << x << std::endl; return; }
-
 int main() {
-	int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense? Awesome tab2[5];
-	Awesome tab2[5];
-	iter( tab, 5, print ); iter( tab2, 5, print );
-return 0;
-}
-
-int paco() {
+	std::cout << "MY TEST" << std::endl;
 	int	iArr[10];
 	float	fArr[10];
 	std::string	strArr[10];
 
 	int	sz = 10;
 
-	::iter(iArr, sz, ::trans);
+	std::cout << "TEST 1" << std::endl;
+	iter(iArr, sz, trans);
 	for (int i = 0; i < sz; i++)
 		std::cout << iArr[i] << std::endl;
 
-	::iter(fArr, sz, ::trans);
+	std::cout << "TEST 2" << std::endl;
+	iter(fArr, sz, trans);
 	for (int i = 0; i < sz; i++)
 		std::cout << fArr[i] << std::endl;
 
-	::iter(strArr, sz, ::trans);
+	std::cout << "TEST 3" << std::endl;
+	iter(strArr, sz, trans);
 	for (int i = 0; i < sz; i++)
 		std::cout << strArr[i] << std::endl;
 	return 0;
